bbfdbe5908765af23317663366384b39cad76bb0
swellee <svtt@163.com>
2025-05-16 21:21:55 +0800

    migration: add user and user-referrence-in-todo

---
+++ server/migrations/20250516131245_add-user-id-in-todos.js
@@ -0,0 +1,20 @@
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('todos', (table)=>{
+    table.integer('user_id').references('users.id')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('todos', (table)=>{
+    table.dropColumn('user_id')
+  })
+};
