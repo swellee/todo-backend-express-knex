2bd6811618f3c1cce9521e466cf348466dff73d5
swellee <svtt@163.com>
2025-05-20 10:21:27 +0800

    todo add content

---
+++ server/migrations/20250517025526_todo-add-content.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('todos', (table) => {
+    table.text('content')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table('todos', (table) => {
+    table.dropColumn('content')
+  })
+};
