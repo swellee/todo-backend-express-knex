54e5df206a8a8c814bf0932c401a98e74c6c4d65
swellee <svtt@163.com>
2025-05-22 12:06:43 +0800

    move routes to individual file

--- server/server.js
+++ server/server.js
@@ -1,28 +1,14 @@
 const app = require('./server-config.js');
-const todo = require('./routes/todo-routes.js');
-const user = require('./routes/user-routes.js')
 const cors = require('cors')
 const authenticate = require('./middleware/authenticate.js')
+const { initRoutes } = require('./routes/index.js');
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors())
 app.use(require('body-parser').json());
 app.use(authenticate);
-
-// todo routes
-app.get('/todo', todo.getAllTodos);
-app.get('/todo/:id', todo.getTodo);
-
-app.post('/todo', todo.postTodo);
-app.patch('/todo/:id', todo.patchTodo);
-
-app.delete('/todo', todo.deleteAllTodos);
-app.delete('/todo/:id', todo.deleteTodo);
-
-// user routes
-app.post('/user/signup', user.signup)
-app.post('/user/login', user.login)
-app.get('/user/me', user.info)
+// init routes
+initRoutes(app);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`Listening on port ${port}`));
